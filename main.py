0b5a559f8a25e34983379eca6eaf822a14503f86
Dima <chaospulse77@gmail.com>
2026-03-29 16:52:03 +0000

    fix bug

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-from logic import calculate_hours; hours = [8,8,7,9]; print(calculate_hours(hours))
+from logic import calculate_hours from validator import validate_hours hours = [8,8,7,9] if validate_hours(hours): print(calculate_hours(hours)) else: print('Invalid data')
