246576fcd5e1a1cda5d10b1329c4c86258c94ecd
Dima <chaospulse77@gmail.com>
2026-03-29 18:23:12 +0000

    fix logging configuration

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 import logging from logic import calculate_hours from validator import validate_hours hours = [8,8,7,9] if validate_hours(hours): logging.info(calculate_hours(hours)) else: logging.error('Invalid data')
 
+=======
+import logging from logic import calculate_hours from validator import validate_hours logging.basicConfig(level-logging.INFO) hours = [8,8,7,9] if validate_hours(hours): logging.info(calculate_hours(hours)) else: logging.error('Invalid data')
+>>>>>>> 5643597 (fix logging configuration)
